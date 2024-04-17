795e7d939fb7ffb0be000b3f180f370152b1593c
Ribeirovisk <franckrm2001@gmail.com>
2024-04-17 20:34:09 -0300

    path module

---
+++ 12_path/index.js
@@ -0,0 +1,7 @@
+const path = require("path")
+
+const customPath = "/relatorios/matheus/relatorio1.pdf"
+
+console.log(path.dirname(custonPath))
+console.log(path.basename(custonPath))
+console.log(path.extname(custonPath))
