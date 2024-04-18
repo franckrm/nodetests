896eb63ca1251a298c6f2851a11611859fd19558
Ribeirovisk <franckrm2001@gmail.com>
2024-04-18 20:02:26 -0300

    learning

---
+++ 13_path_absoluto/index.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+
+
+const midFolder = "relatorios"
+const fileName = 'matheus.txt'
+
+const finalPath = path.join('suamar','/ddd', 'arquivos', midFolder, fileName)
+
+console.log(finalPath)
